c0d5c5b5ea5560537639d44b4503cee2dbd327b4
Meredith Reece <mardebugger@gmail.com>
2018-03-08 00:29:13 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -11,9 +11,15 @@ class FiguresController < ApplicationController
   post '/figures' do
     @figure = Figure.create(params[:figure])
     if !params[:landmark][:name].empty?
+<<<<<<< HEAD
       @figure.landmarks << Landmark.create(name: params[:landmark][:name])
     elsif !params[:title][:name].empty?
       @figure.titles << Title.create(name: params[:title][:name])
+=======
+      @figure.landmarks << Landmark.find_or_create_by(name: params[:landmark][:name])
+    elsif !params[:title][:name].empty?
+      @figure.titles << Title.find_or_create_by(name: params[:title][:name])
+>>>>>>> b08c07d419e027728076727b1a8904bebe78dec9
     end
     redirect '/figures'
   end
@@ -25,6 +31,7 @@ class FiguresController < ApplicationController
 
   get '/figures/:id/edit' do
     @figure = Figure.find_by(id: params[:id])
+<<<<<<< HEAD
     erb :'figures/edit'
   end
 
@@ -42,5 +49,14 @@ class FiguresController < ApplicationController
 
     @figure.save
     redirect "/figures/#{@figure.id}"
+=======
+    binding.pry
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find_by(id: params[:id])
+    # binding.pry
+>>>>>>> b08c07d419e027728076727b1a8904bebe78dec9
   end
 end
--- config.ru
+++ config.ru
@@ -6,7 +6,11 @@ use Rack::Static, :urls => ['/css'], :root => 'public' # Rack fix allows seeing 
 if defined?(ActiveRecord::Migrator) && ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending run `rake db:migrate` to resolve the issue.'
 end
+<<<<<<< HEAD
 use Rack::MethodOverride
+=======
+
+>>>>>>> b08c07d419e027728076727b1a8904bebe78dec9
 use FiguresController
 use LandmarksController
 run ApplicationController
