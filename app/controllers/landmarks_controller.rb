ef077c7d84a6cf617b10be777367dd1262808a92
Meredith Reece <nerdybug@gmail.com>
2018-03-07 20:07:31 -0500

    Update landmarks_controller.rb

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,34 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    erb :'landmarks/new'
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find_by(id: params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find_by(id: params[:id])
+    @landmark.name = params[:landmark][:name] if params[:landmark][:name]
+    @landmark.year_completed = params[:landmark][:year_completed] if params[:landmark][:year_completed]
+    @landmark.save
+    redirect "/landmarks/#{@landmark.id}"
+  end
 end
+
